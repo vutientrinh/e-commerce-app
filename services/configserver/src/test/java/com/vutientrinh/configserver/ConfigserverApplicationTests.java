8a7ed5616066f598acc4c64a220975791f528307
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-01-01 20:50:35 +0700

    config server and delivery

---
+++ services/configserver/src/test/java/com/vutientrinh/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vutientrinh.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
