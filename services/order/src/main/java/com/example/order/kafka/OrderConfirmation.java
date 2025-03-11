219bdbcbe1f92998f593abee9f780a091e14351f
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-03-11 22:40:38 +0700

    order producer

---
+++ services/order/src/main/java/com/example/order/kafka/OrderConfirmation.java
@@ -0,0 +1,17 @@
+package com.example.order.kafka;
+
+import com.example.order.customer.CustomerResponse;
+import com.example.order.product.PaymentMethod;
+import com.example.order.product.PurchaseResponse;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record OrderConfirmation(
+        String orderReference,
+        BigDecimal totalAmount,
+        PaymentMethod paymentMethod,
+        CustomerResponse customerResponse,
+        List<PurchaseResponse> products
+) {
+}
