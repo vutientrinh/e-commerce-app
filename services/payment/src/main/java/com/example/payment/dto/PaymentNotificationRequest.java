eb00acc2592421fcc59bce273a515235f034d16f
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-03-14 11:40:17 +0700

    payment service notification

---
+++ services/payment/src/main/java/com/example/payment/dto/PaymentNotificationRequest.java
@@ -0,0 +1,15 @@
+package com.example.payment.dto;
+
+import com.example.payment.util.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstName,
+        String customerLastName,
+        String customerEmail
+) {
+}
--- services/payment/src/main/java/com/example/payment/service/PaymentService.java
+++ services/payment/src/main/java/com/example/payment/service/PaymentService.java
@@ -1,7 +1,9 @@
 package com.example.payment.service;
 
 
+import com.example.payment.dto.PaymentNotificationRequest;
 import com.example.payment.dto.PaymentRequest;
+import com.example.payment.notification.NotificationProducer;
 import com.example.payment.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,8 +13,20 @@ import org.springframework.stereotype.Service;
 public class PaymentService {
     private final PaymentRepository paymentRepository;
     private final PaymentMapper paymentMapper;
+    private NotificationProducer notificationProducer;
     public Integer createPayment(PaymentRequest paymentRequest) {
         var payment = paymentRepository.save(paymentMapper.toPayment(paymentRequest));
-        return null;
+
+        notificationProducer.sendNotification(
+                new PaymentNotificationRequest(
+                        paymentRequest.orderReference(),
+                        paymentRequest.amount(),
+                        paymentRequest.paymentMethod(),
+                        paymentRequest.customer().firstName(),
+                        paymentRequest.customer().lastName(),
+                        paymentRequest.customer().email()
+                )
+        );
+        return payment.getId();
     }
 }
