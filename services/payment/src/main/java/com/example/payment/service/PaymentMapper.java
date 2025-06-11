b28e6631b37a9ff350cd3bb55031c7547c1269bd
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-06-11 16:57:46 +0700

    order and payment

--- services/payment/src/main/java/com/example/payment/service/PaymentMapper.java
+++ services/payment/src/main/java/com/example/payment/service/PaymentMapper.java
@@ -2,7 +2,9 @@ package com.example.payment.service;
 
 import com.example.payment.dto.PaymentRequest;
 import com.example.payment.payment.Payment;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PaymentMapper {
     public Payment toPayment(PaymentRequest paymentRequest) {
         return Payment.builder()
